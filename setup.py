514de981dc2904bc9e4f5dde998b43185845e1c3
GabrielDev <gabrys.wisniewski@op.pl>
2025-05-17 20:45:44 +0200

    add: new version 1.0.7 / fix: annotation cheker ( mainly - data types )

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="yololint",
-    version="1.0.3",
+    version="1.0.7",
     description="YOLO Dataset Debugger (yololint) is a tool for automatic validation and diagnostics of YOLO-format datasets. It helps you quickly detect common errors, inconsistencies, and missing files in your dataset structure and annotations before you start model training. With clear reports and easy usage, you save time and ensure your dataset is ready for deep learning projects.",
     long_description=long_description,
     long_description_content_type="text/markdown",
